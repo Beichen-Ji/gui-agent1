b98759586b45ad4647d45798332442c61a206ead
Beichen-Ji <bji7@uwo.ca>
2026-08-17 05:14:48 -0400

    feat: add cross-platform screen capture

---
+++ src/gui_agent/perception/__init__.py
@@ -0,0 +1,13 @@
+from gui_agent.perception.capture import (
+    CaptureError,
+    InvalidMonitorError,
+    InvalidRegionError,
+    ScreenCapture,
+)
+
+__all__ = [
+    "CaptureError",
+    "InvalidMonitorError",
+    "InvalidRegionError",
+    "ScreenCapture",
+]
